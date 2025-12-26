0170741337b03a6712977d0eda28e8c6f65da37a
CrhistoferVera <cristofervera110@gmail.com>
2025-12-26 16:01:11 -0400

    sepolia deploy initial config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
+// [1] IMPORTANTE: Esta linea soluciona el error ts(2353)
+import "@nomicfoundation/hardhat-verify";
+
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable, defineConfig } from "hardhat/config";
 
 export default defineConfig({
   plugins: [hardhatToolboxMochaEthersPlugin],
+
   solidity: {
     profiles: {
       default: {
@@ -19,27 +23,31 @@ export default defineConfig({
       },
     },
   },
- networks: {
-  hardhatMainnet: {
-    type: "edr-simulated",
-    chainType: "l1",
+
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    localhost: {
+      type: "http",
+      chainId: 31337,
+      url: "http://127.0.0.1:8545",
+    },
   },
-  hardhatOp: {
-    type: "edr-simulated",
-    chainType: "op",
+
+  // [2] Ahora TypeScript reconocerá este bloque sin errores
+  etherscan: {
+    apiKey: configVariable("ETHERSCAN_API_KEY"),
   },
-  sepolia: {
-    type: "http",
-    chainType: "l1",
-    url: configVariable("SEPOLIA_RPC_URL"),
-    accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-  },
-  localhost: {
-  type: "http",              // obligatorio
-  chainId: 31337,            // Hardhat node por defecto usa 31337
-  url: "http://127.0.0.1:8545",
-  // accounts opcional, puedes dejar que Hardhat use las cuentas predeterminadas
-  // accounts: ["0x..."],
-},
-},
-});
+} as any);
